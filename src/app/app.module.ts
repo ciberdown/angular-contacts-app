8b679f90780dd52d3e4a20207892b8c9f153bf25
ciberdown <teymuri.amin@gmail.com>
2023-10-25 02:59:19 +0330

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { InputComponent } from './main/input/input.component';
 import { OutputComponent } from './main/output/output.component';
 import { FlexboxComponent } from './customComponents/flexbox/flexbox.component';
 import { GridComponent } from './customComponents/grid/grid.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'contacts',
+    component: MainComponent,
+  },
+];
 @NgModule({
-  declarations: [AppComponent, MainComponent, InputComponent, OutputComponent, FlexboxComponent, GridComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    InputComponent,
+    OutputComponent,
+    FlexboxComponent,
+    GridComponent,
+    HomeComponent,
+    NavigationComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
